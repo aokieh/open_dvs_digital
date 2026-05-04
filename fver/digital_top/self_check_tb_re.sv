2bfc10a63d5315ab16579ec344f22c0f66d72ebf
aokieh <ababakaro@gmail.com>
2026-05-04 00:17:17 -0400

    purely rising edge spi

---
+++ fver/digital_top/self_check_tb_re.sv
@@ -0,0 +1,264 @@
+`timescale 1ns/1ps
+import pkg_spi_fver::*;
+
+module tb ();
+
+    localparam CLK_P = 20ns;
+    localparam DEPTH = 8;
+    localparam DEASSERT_THRESH = 11;
+    localparam ASSERT_THRESH   = 789;
+
+    logic clk  = 0;
+    logic rst_n = 0;
+
+    // SPI Interface
+    logic CS_N;
+    logic SCK;
+    logic [3:0] COPI;
+    logic [3:0] CIPO;
+
+    // Regfile interface
+    logic [`BIAS_WIDTH-1:0] biases [`NUM_BIASES-1:0];    
+    logic [`DAC_WIDTH-1:0] dac_configs [`NUM_DACS-1:0];
+
+    // some are removable signals that we aren't using (in the end)
+    logic                        we_out;
+    logic [`FIFO_AWIDTH-1:0]     irq_assert_thresh;
+    logic [`FIFO_AWIDTH-1:0]     irq_deassert_thresh;
+    logic [`FIFO_AWIDTH-1:0]     fifo_numel;
+    logic                        fifo_rd_en;
+    logic                        fifo_rst_n;
+    logic [3:0]                  digit;
+
+
+    // TODO (REMOVE?): Ports on behavioral models, gave us issues?
+    supply1 VDD; // Ideal 1 (Power Source)
+    supply0 VSS; // Ideal 0 (Ground Source)
+    wire vccd1; // Core Power Net (VCC)
+    wire vssd1; // Core Ground Net (VSS)
+    // assign VPB   = VDD; // Tie P-Well bias (VPB) to VDD
+    // assign VNB   = VSS; // Tie N-Well bias (VNB) to VSS
+
+    assign vccd1 = VDD;
+    assign vssd1 = VSS;
+
+    // Registers for error checking
+    logic [11:0] dac_write_data     [7:0];
+    logic [11:0] dac_read_data      [7:0];
+
+    logic [23:0] bias_write_data    [3:0];
+    logic [23:0] bias_read_data     [3:0];
+
+    // Example on assigning random data to registers
+    logic [11:0] irq_deassert_write_val = $random & 10'h3FF;
+    logic [11:0] irq_assert_write_val   = $random & 10'h3FF;
+
+    spi_intf i_spi_intf(
+        .CS_N,
+        .SCK ,
+        .COPI,
+        .CIPO
+    );
+
+    class_spi_ctrl spi_ctrl = new (i_spi_intf);
+
+    always #(CLK_P/2) clk = ~clk;
+
+    digital_top_re i_digital_top (
+        .clk         (clk),
+        .rst_n       (rst_n),
+        .vccd1       (vccd1),
+        .vssd1       (vssd1),
+        .CS_N        (CS_N),
+        .SCK         (SCK),
+        .COPI        (COPI),
+        .CIPO        (CIPO),
+        .bias_0      (biases[0]),
+        .bias_1      (biases[1]),
+        .bias_2      (biases[2]),
+        .bias_3      (biases[3]),
+        .we_out      (we_out),
+        .dac_config_0(dac_configs[0]),
+        .dac_config_1(dac_configs[1]),
+        .dac_config_2(dac_configs[2]),
+        .dac_config_3(dac_configs[3]),
+        .dac_config_4(dac_configs[4]),
+        .dac_config_5(dac_configs[5]),
+        .dac_config_6(dac_configs[6]),
+        .dac_config_7(dac_configs[7]),
+        .irq_assert_thresh_reg(irq_assert_thresh),
+        .irq_deassert_thresh_reg(irq_deassert_thresh),
+        .fifo_numel_reg        (fifo_numel),
+        .fifo_rd_en_reg        (fifo_rd_en),
+        .fifo_rst_n_reg        (fifo_rst_n),
+        .shift_en_fifo(),
+        .rdata_spi_0(16'h0000),
+        .rdata_spi_1(16'h0000)
+    );
+
+    // ---------------- Tasks and Verification Sequences ------------------
+
+    task automatic pulse_fifo_rst_n(input logic [3:0] val);
+        spi_ctrl.trans(WRITE_BT, 1, val);
+        #CLK_P;
+    endtask
+
+    task automatic set_irq(input logic [11:0] deassert_val, input logic [11:0] assert_val, input logic mode_read);
+            spi_ctrl.trans(WRITE_HW, 12, deassert_val);
+            #CLK_P;
+            spi_ctrl.trans(WRITE_HW, 14, assert_val);
+            #CLK_P;
+        if(mode_read) begin
+            spi_ctrl.trans(READ_HW, 12, 0, deassert_val);
+            #CLK_P;
+            spi_ctrl.trans(READ_HW, 14, 0, assert_val);
+            #CLK_P;
+        end 
+    endtask
+
+    task automatic write_dacs(input logic [11:0] val, input logic mode_read);
+        for (int i = 0; i < `NUM_DACS; i++) begin
+            spi_ctrl.trans(WRITE_HW, i*2 + 20, val);
+            dac_write_data[i] = val;
+            #CLK_P;
+        end
+        if (mode_read) begin
+            for (int i = 0; i < `NUM_DACS; i++) begin
+                spi_ctrl.trans(READ_HW, i*2 + 20, 0, val);
+                // dac_write_data[i] = val;
+                #CLK_P;
+            end
+        end
+    endtask
+
+    task automatic read_dacs(input logic [11:0] val);
+        for (int i = 0; i < `NUM_DACS; i++) begin
+            spi_ctrl.trans(WRITE_HW, i*2 + 20, val+i);
+            dac_write_data[i] = val;
+            #CLK_P;
+        end
+        
+        for (int i = 0; i < `NUM_DACS; i++) begin
+            spi_ctrl.trans(READ_HW, i*2 + 20, 0, val+i);
+            #CLK_P;
+        end
+    endtask
+
+    task automatic write_dacs_seq(input logic [11:0] val);
+        for (int i = 0; i < 10; i++) begin
+            spi_ctrl.trans(WRITE_HW, i*2 + 20, val+i);
+            dac_write_data[i] = val + i;
+            #CLK_P;
+        end
+    endtask
+
+    task automatic write_biases(input logic [3:0] start_val, input logic is_uniform, input logic mode_read);
+        logic [3:0] digit;
+        logic [23:0] bias_val;
+        for (int i = 0; i < `NUM_BIASES; i++) begin
+            if (is_uniform)
+                digit = start_val;
+            else
+                digit = (start_val + i) & 4'hF;
+
+            bias_val = {6{digit}};
+            spi_ctrl.trans(WRITE_WD, 112 + i*4, bias_val);
+            bias_write_data[i] = bias_val;
+            $display("Bias[%0d] write = %06h", i, bias_val);
+            #CLK_P;
+        end
+
+        for (int i = 0; i < `NUM_BIASES; i++) begin
+            if(mode_read) begin
+                if (is_uniform)
+                    digit = start_val;
+                else
+                    digit = (start_val + i) & 4'hF;
+                bias_val = {6{digit}};
+                spi_ctrl.trans(READ_WD, 112 + i*4, 0, bias_val);
+                bias_write_data[i] = bias_val;
+                $display("Bias[%0d] write = %06h", i, bias_val);
+                #CLK_P;
+            end
+        end
+    endtask
+
+    // --------------------- Test Sequence ------------------------------
+    initial begin
+        //local sdf folder
+        
+                // For corner: max_ff_n40C_1v95
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/max_ff_n40C_1v95/digital_top__max_ff_n40C_1v95.sdf", i_digital_top);
+        
+                // For corner: max_ss_100C_1v60
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/max_ss_100C_1v60/digital_top__max_ss_100C_1v60.sdf", i_digital_top);
+        
+                // For corner: max_tt_025C_1v80
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/max_tt_025C_1v80/digital_top__max_tt_025C_1v80.sdf", i_digital_top);
+        
+                // For corner: min_ff_n40C_1v95
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/min_ff_n40C_1v95/digital_top__min_ff_n40C_1v95.sdf", i_digital_top);
+        
+                // For corner: min_ss_100C_1v60
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/min_ss_100C_1v60/digital_top__min_ss_100C_1v60.sdf", i_digital_top);
+        
+                // For corner: min_tt_025C_1v80
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/min_tt_025C_1v80/digital_top__min_tt_025C_1v80.sdf", i_digital_top);
+        
+                // For corner: nom_ff_n40C_1v95
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/nom_ff_n40C_1v95/digital_top__nom_ff_n40C_1v95.sdf", i_digital_top);
+        
+                // For corner: nom_ss_100C_1v60
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/nom_ss_100C_1v60/digital_top__nom_ss_100C_1v60.sdf", i_digital_top);
+        
+                // For corner: nom_tt_025C_1v80
+        // $sdf_annotate("/home/aokieh1/projects/digital_top_hardened_macro/openlane/digital_top/runs/antenna_clean/final/sdf/nom_tt_025C_1v80/digital_top__nom_tt_025C_1v80.sdf", i_digital_top);
+        
+        spi_ctrl.init();
+
+        // Reset sequence
+        #(10*CLK_P); rst_n = 1;
+        #(10*CLK_P); rst_n = 0;
+        #(10*CLK_P); rst_n = 1;
+        #(5*CLK_P);
+
+        // ---------------- Write all ones ------------------------
+        pulse_fifo_rst_n('hf);
+        set_irq('hfff, 'hfff, 0);
+        write_dacs('hfff, 0);
+        write_biases(24'hffffff, 1, 0);
+        #500ns;
+
+        // ---------------- Write all zeros -----------------------
+        pulse_fifo_rst_n('h0);
+        set_irq('h000, 'h000, 0);
+        write_dacs('h000, 0);
+        write_biases(4'h0, 1, 0);
+        #500ns;
+
+        // ---------------- Write sequence data -------------------
+        write_dacs_seq('h5aa);
+        write_biases(4'ha, 0, 0);              // starts A, increments
+        set_irq('h2AA, 'h2AA, 0);
+        #500ns;
+
+        // ---------------- Write sequence data -------------------
+        write_dacs_seq('hfaa);
+        write_biases(4'hf, 0, 0);              // starts F, increments
+        set_irq('h0CC, 'h1DD, 0);
+        #500ns;
+
+        // ---------------- Read and dump comparison --------------
+        // Read Chip ID
+        spi_ctrl.trans(READ_BT, 0, 0, 'h55);
+        #CLK_P;
+
+        set_irq(irq_deassert_write_val, irq_assert_write_val, 1);
+        read_dacs('h100);   //writes consecutive data to dacs & reads
+        write_biases(4'h3, 0, 1);  //write and read beginning at 333333
+
+        #300ns;
+        $stop;
+    end
+
+endmodule : tb
---
+++ source/design/final_macros/digital_top_re.sv
@@ -0,0 +1,184 @@
+
+module digital_top_re (
+    `ifdef USE_POWER_PINS
+        inout vccd1, 
+        inout vssd1, 
+    `endif
+    input  logic clk,
+    input  logic rst_n,
+    //Added for ASIC tools
+    // input  logic vccd1,  // OpenLane Power  - comment out if needed
+    // input  logic vssd1,  // OpenLane Ground - comment out if needed
+    // SPI Interface
+    input  logic CS_N,
+    input  logic SCK,
+    input  logic [3:0] COPI,
+    output logic [3:0] CIPO,
+    output logic [23:0] bias_0,
+    output logic [23:0] bias_1,
+    output logic [23:0] bias_2,
+    output logic [23:0] bias_3,
+    // Added DAC outputs
+    output logic [`DAC_WIDTH-1:0] dac_config_0,
+    output logic [`DAC_WIDTH-1:0] dac_config_1,
+    output logic [`DAC_WIDTH-1:0] dac_config_2,
+    output logic [`DAC_WIDTH-1:0] dac_config_3,
+
+    output logic [`DAC_WIDTH-1:0] dac_config_4,
+    output logic [`DAC_WIDTH-1:0] dac_config_5,
+    output logic [`DAC_WIDTH-1:0] dac_config_6,
+    output logic [`DAC_WIDTH-1:0] dac_config_7,
+    output logic [`DAC_WIDTH-1:0] dac_config_8,
+    output logic [`DAC_WIDTH-1:0] dac_config_9,
+
+    // TODO: removable signals that we aren't using (at the moment)
+    output logic                        we_out,
+    // output logic                        we_reg,
+    output logic [`FIFO_AWIDTH-1:0]     irq_assert_thresh_reg,
+    output logic [`FIFO_AWIDTH-1:0]     irq_deassert_thresh_reg,
+    input  logic [`FIFO_AWIDTH-1:0]     fifo_numel_reg,
+    output logic                        fifo_rd_en_reg,
+    output logic                        fifo_rst_n_reg,
+
+    output logic [7:0] event_rate_reg,
+
+    //FIFO Interface (SPI <---> FIFO)
+    input  logic [15:0] rdata_spi_0,
+    input  logic [15:0] rdata_spi_1,
+    output logic [1:0] shift_en_fifo
+);
+
+
+    // Memory Interface
+    logic                  we_reg;
+    logic [`RF_AWIDTH-1:0] addr_reg;
+    logic [ `RF_WIDTH-1:0] wdata_reg;
+    logic [  `RF_MASK-1:0] wmask_reg;
+    logic [ `RF_WIDTH-1:0] rdata_reg;
+
+    // FIFO registers
+    // logic                    fifo_rst_n;
+    // logic                    fifo_rd_en;
+    // logic [`FIFO_AWIDTH-1:0] fifo_numel = 10'h3FF;//TODO remove assignment
+
+    // IRQ registers
+    logic [`FIFO_AWIDTH-1:0] irq_deassert_thresh;
+    // logic [`FIFO_AWIDTH-1:0] irq_assert_thresh;
+
+    // DAC registers
+    logic [`DAC_WIDTH-1:0] dac_config [`NUM_DACS];
+    assign dac_config[0] = dac_config_0;
+    assign dac_config[1] = dac_config_1;
+    assign dac_config[2] = dac_config_2;
+    assign dac_config[3] = dac_config_3;
+
+    assign dac_config[4] = dac_config_4;
+    assign dac_config[5] = dac_config_5;
+    assign dac_config[6] = dac_config_6;
+    assign dac_config[7] = dac_config_7;
+
+    assign dac_config[8] = dac_config_8;
+    assign dac_config[9] = dac_config_9;
+    // logic [`DAC_WIDTH-1:0] dac_config_0; 
+    // logic [`DAC_WIDTH-1:0] dac_config_1;
+    // logic [`DAC_WIDTH-1:0] dac_config_2;
+    // logic [`DAC_WIDTH-1:0] dac_config_3;
+
+    // logic [`DAC_WIDTH-1:0] dac_config_4; 
+    // logic [`DAC_WIDTH-1:0] dac_config_5;
+    // logic [`DAC_WIDTH-1:0] dac_config_6;
+    // logic [`DAC_WIDTH-1:0] dac_config_7;
+
+    //ADDITIONAL SIGNALS - test registers to test ports
+    logic [23:0] bias [`NUM_BIASES];
+    // logic [9:0] event_rate = 10'h3FF; //TODO (remove) gets written to mem[27]
+
+    // hard wiring the added memory addresses
+    // assign bias[0] = 24'hAAA;
+    // assign bias[1] = 24'hBBB;
+    // assign bias[2] = 24'hCCC;
+    // assign bias[3] = 24'hDDD;
+    assign bias[3] = bias_3; // removed for yosys
+    assign bias[2] = bias_2;
+    assign bias[1] = bias_1;
+    assign bias[0] = bias_0;
+    //---------------------------------------------------
+    // SPI Peripheral
+    //---------------------------------------------------
+    spi_peripheral_re i_spi_peripheral (
+        `ifdef USE_POWER_PINS
+            .vccd1             (vccd1),
+            .vssd1             (vssd1),
+        `endif
+        .CS_N,
+        .SCK,
+        .COPI,
+        .CIPO,
+        
+        .addr_reg,
+        .we_reg,
+        .we_out,       //TODO: remove, set as test wire for debug
+        .wdata_reg,
+        .wmask_reg,
+        .rdata_reg,
+
+        .rdata_spi_0(rdata_spi_0),
+        .rdata_spi_1(rdata_spi_1),
+        .shift_en_fifo(shift_en_fifo),
+        .data_ready_spi(1'b0) // Tied off for testing, or route to a top-level port
+    );
+
+
+    //---------------------------------------------------
+    // Register File
+    //---------------------------------------------------
+    regfile i_regfile(
+
+        `ifdef USE_POWER_PINS
+            .vccd1             (vccd1),
+            .vssd1             (vssd1),
+        `endif
+
+        .clk,
+        .rst_n,
+
+        // Memory Interface (SPI <-> Mem)
+        .addr_reg,
+        .we_reg,
+        .wdata_reg,
+        .wmask_reg,
+        .rdata_reg,
+
+        // FIFO
+        .fifo_rst_n_reg,
+        // input  logic                    fifo_empty,
+        // input  logic                    fifo_full,
+        .fifo_rd_en_reg,
+        .fifo_numel_reg,
+        // input  logic [ `FIFO_WIDTH-1:0] fifo_rdata,
+
+        // IRQ
+        .irq_deassert_thresh_reg,
+        .irq_assert_thresh_reg,
+
+        // DAC
+        // output logic [`DAC_WIDTH-1:0] dac_config [`NUM_DACS],
+        .dac_config_0,
+        .dac_config_1,
+        .dac_config_2,
+        .dac_config_3,
+        .dac_config_4, 
+        .dac_config_5,
+        .dac_config_6,
+        .dac_config_7,
+
+        //TEST ADDITIONAL PORTS
+        .bias_0,
+        .bias_1,
+        .bias_2,
+        .bias_3,
+
+        .event_rate_reg
+);
+
+endmodule : digital_top_re
